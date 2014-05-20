509f1740a262a8da6d6f41de2d303a47e1c844e9
Salvatore Di Guida <salvatore.di.guida@cern.ch>
2014-05-20 19:13:06 +0200

    Use FilterUnit in tests.

--- DQMServices/Examples/python/test/DQMExample_Step1_cfg.py
+++ DQMServices/Examples/python/test/DQMExample_Step1_cfg.py
@@ -40,13 +40,19 @@ process.DQMoutput = cms.OutputModule("DQMRootOutputModule",
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:mc', '')  #for MC
 
+#DQMFileSaver configuration
+process.dqmSaver.saveByLumiSection = cms.untracked.int32(1)
+process.dqmSaver.convention = cms.untracked.string('FilterUnit')
+process.dqmSaver.fileFormat = cms.untracked.string('ROOT')
+process.dqmSaver.workflow = cms.untracked.string('')
+
 # Path and EndPath definitions
-process.dqmoffline_step = cms.Path(process.DQMExample_Step1)
-process.DQMoutput_step = cms.EndPath(process.DQMoutput)
+process.dqmoffline_step = cms.Path(process.DQMExample_Step1+process.DQMSaver)
+#process.DQMoutput_step = cms.EndPath(process.DQMoutput)
 
 
 # Schedule definition
 process.schedule = cms.Schedule(
     process.dqmoffline_step,
-    process.DQMoutput_step
+#    process.DQMoutput_step
     )
